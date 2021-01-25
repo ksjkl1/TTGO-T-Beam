58d6d5fbfc014ddeb8088c19323a41e3020028d3
ksjkl1 <hans.kleijn@outlook.com>
2021-01-25 08:33:20 +0100

    ttgo

--- main/ttn.ino
+++ main/ttn.ino
@@ -1,27 +1,20 @@
 /*
-
 TTN module
 Wrapper to use TTN with the LMIC library
-
 Copyright (C) 2018 by Xose Pérez <xose dot perez at gmail dot com>
-
 This code requires the MCCI LoRaWAN LMIC library
 by IBM, Matthis Kooijman, Terry Moore, ChaeHee Won, Frank Rose
 https://github.com/mcci-catena/arduino-lmic
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 */
 
 #include <hal/hal.h>
