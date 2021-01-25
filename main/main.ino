63e0765ae9312ee762a8bd8bf88171baafbc7e02
Hans Kleijn <ksjkl1@zoe-n-hansdebian.debian.lan>
2021-01-25 08:24:38 +0100

    ttgo

--- main/main.ino
+++ main/main.ino
@@ -1,24 +1,17 @@
-/*  
-
+/*
   Main module
-
   # Modified by Kyle T. Gabriel to fix issue with incorrect GPS data for TTNMapper
-
   Copyright (C) 2018 by Xose Pérez <xose dot perez at gmail dot com>
-
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 */
 
 #include "configuration.h"
@@ -74,7 +67,8 @@ bool trySend() {
     buildPacket(txBuffer);
 
 #if LORAWAN_CONFIRMED_EVERY > 0
-    bool confirmed = (count % LORAWAN_CONFIRMED_EVERY == 0);
+    bool confirmed = (ttn_get_count() % LORAWAN_CONFIRMED_EVERY == 0);
+    if (confirmed){ Serial.println("confirmation enabled"); }
 #else
     bool confirmed = false;
 #endif
@@ -298,7 +292,6 @@ void initDeepSleep() {
     wakeCause = esp_sleep_get_wakeup_cause(); 
     /* 
     Not using yet because we are using wake on all buttons being low
-
     wakeButtons = esp_sleep_get_ext1_wakeup_status();       // If one of these buttons is set it was the reason we woke
     if (wakeCause == ESP_SLEEP_WAKEUP_EXT1 && !wakeButtons) // we must have been using the 'all buttons rule for waking' to support busted boards, assume button one was pressed
         wakeButtons = ((uint64_t)1) << buttons.gpios[0];
